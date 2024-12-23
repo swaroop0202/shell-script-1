48472104fe73df828414a9f1a3d0fdb2177d2280
swaroop0202 <swaroopk372@gmail.com>
2024-12-23 17:07:16 +0530

    M

--- 04-specialvars.sh
+++ 04-specialvars.sh
@@ -5,3 +5,10 @@ Css=Cs
 
 echo "all variables: $@"
 echo "no.of variables: $#"
+echo "script name:$0"
+echo "current working directory:$PWD"
+echo "home directory of the current user: $HOME"
+echo "which user is running this script: $USER"
+echo "hostname: $HOSTNAME"
+echo "process ID of the current shellscript: $$"
+echo "process ID of the background command: $!"
