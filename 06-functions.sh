a843efad59f64c93d307dd6cfaf233b163413204
swaroop0202 <swaroopk372@gmail.com>
2024-12-24 13:43:23 +0530

    M

--- 06-functions.sh
+++ 06-functions.sh
@@ -2,11 +2,13 @@
 
 USERID=$(id -u)
 
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$( date +%F-%H-%M-%S)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+
 if [ $USERID -ne 0 ]
 then 
     echo "please run this script with root user"
 else
     echo "you are a root user"
 fi
-
-SCRIPTNAME=$(echo $0 | cut -d "." -f1)
