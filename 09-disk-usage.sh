76d1710ac547f8c77098e3276535626b4c889644
swaroop0202 <swaroopk372@gmail.com>
2025-01-04 18:41:46 +0530

    M

--- 09-disk-usage.sh
+++ 09-disk-usage.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-DISK_FOLDER=$(df -hT | grep xfs | awk -F " " '{print $7F}'
-)
-
-DISK_USAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d '%' -f1
-)
-
+DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=6
 
-if [ $(DISK_USAGE) -gt $(DISK_THRESHOLD) ]
-then
-    echo "$DISK_FOLDER is greater than $DISK_USAGE , current usage=$DISK_USAGE"
-else
-    echo "$DISK_FOLDER is less  than $DISK_USAGE , current usage=$DISK_USAGE"
-fi
+while IFS= read -r line
+do
+    usage=$(echo $line | awk -F " " '(print $6F)' )
+    folder=$(echo $line |awk -F " " '(print $NF)')
+    if [ $usage -ge $DISK_THRESHOLD ]
+    then
+        echo "$usage is more than $DISK_TRESHOLD, current usage is $usage"
+done <<< $DISK_USAGE
