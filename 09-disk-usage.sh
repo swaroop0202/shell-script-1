f0713273b534e9860bc73b2aa82424219ddd545b
swaroop0202 <swaroopk372@gmail.com>
2025-01-04 18:45:55 +0530

    M

--- 09-disk-usage.sh
+++ 09-disk-usage.sh
@@ -5,8 +5,8 @@ DISK_THRESHOLD=6
 
 while IFS= read -r line
 do
-    usage=$(echo $line | awk -F " " '(print $6F)' | cut -d "%" -f1 )
-    folder=$(echo $line |awk -F " " '(print $NF)')
+    usage=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+    folder=$(echo $line |awk -F " " '{print $NF}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
         echo "$usage is more than $DISK_TRESHOLD, current usage is $usage"
